cd64f6914f5febc9c9c44982c072bdfe9a07f6e9
lucafluri <luca.fluri4@gmail.com>
2020-10-12 22:27:20 +0000

    working collision

--- Control3DProgram.py
+++ Control3DProgram.py
@@ -15,19 +15,20 @@ from Matrices import *
 class GraphicsProgram3D:
     # Array of Walls
     # [x, y, x-length, y-length]
-    walls = [[0, 0.0, 0.1, -4], [2.5, 2, 5, 0.1], [5, 3.0, 0.1, 2], 
-            [4.0, 4, -2, 0.1], [3, 5.0, 0.1, 2], [4.0, 6, 2, 0.1], 
-            [5, 7.0, 0.1, 2], [8.0, 8, 6, 0.1], [11, 6.0, 0.1, -4], 
-            [10.0, 4, -2, 0.1], [9, 5.0, 0.1, 2], [8.0, 6, -2, 0.1], 
-            [7, 4.0, 0.1, -4], [8.5, 2, 3, 0.1], [10, -2.0, 0.1, -8], 
-            [11.5, -6, 3, 0.1], [13, -3.0, 0.1, 6], [14.0, 0, 2, 0.1], 
-            [15, -4.0, 0.1, -8], [12.5, -8, -5, 0.1], [10, -9.0, 0.1, -2], 
-            [9.0, -10, -2, 0.1], [8, -9.0, 0.1, 2], [6.5, -8, -3, 0.1], 
-            [5, -10.5, 0.1, -5], [4.0, -13, -2, 0.1], [3, -12.0, 0.1, 2], 
-            [2.0, -11, -2, 0.1], [1, -10.0, 0.1, 2], [2.0, -9, 2, 0.1], 
-            [3, -7.5, 0.1, 3], [2.0, -6, -2, 0.1], [1, -5.0, 0.1, 2], 
-            [3.0, -4, 4, 0.1], [5, -5.0, 0.1, -2], [6.5, -6, 3, 0.1], 
-            [8, -4.0, 0.1, 4], [4.0, -2, -8, 0.1]]
+    thickness = 0.01
+    walls = [[0, 0.0, thickness, 4], [2.5, 2, 5, thickness], [5, 3.0, thickness, 2], 
+            [4.0, 4, 2, thickness], [3, 5.0, thickness, 2], [4.0, 6, 2, thickness], 
+            [5, 7.0, thickness, 2], [8.0, 8, 6, thickness], [11, 6.0, thickness, 4], 
+            [10.0, 4, 2, thickness], [9, 5.0, thickness, 2], [8.0, 6, 2, thickness], 
+            [7, 4.0, thickness, 4], [8.5, 2, 3, thickness], [10, -2.0, thickness, 8], 
+            [11.5, -6, 3, thickness], [13, -3.0, thickness, 6], [14.0, 0, 2, thickness], 
+            [15, -4.0, thickness, -8], [12.5, -8, 5, thickness], [10, -9.0, thickness, 2], 
+            [9.0, -10, 2, thickness], [8, -9.0, thickness, 2], [6.5, -8, 3, thickness], 
+            [5, -10.5, thickness, 5], [4.0, -13, 2, thickness], [3, -12.0, thickness, 2], 
+            [2.0, -11, 2, thickness], [1, -10.0, thickness, 2], [2.0, -9, 2, thickness], 
+            [3, -7.5, thickness, 3], [2.0, -6, 2, thickness], [1, -5.0, thickness, 2], 
+            [3.0, -4, 4, thickness], [5, -5.0, thickness, 2], [6.5, -6, 3, thickness], 
+            [8, -4.0, thickness, 4], [4.0, -2, 8, thickness]]
 
 
     def __init__(self):
@@ -59,7 +60,6 @@ class GraphicsProgram3D:
 
         self.angle = 0
 
-        self.UP_key_down = False  
         self.W_key_down = False  
         self.S_key_down = False  
         self.A_key_down = False  
@@ -70,7 +70,7 @@ class GraphicsProgram3D:
         self.G_key_down = False
         self.LEFT_key_down = False
         self.RIGHT_key_down = False
-        self.TOP_key_down = False
+        self.UP_key_down = False
         self.DOWN_key_down = False
 
 
@@ -80,8 +80,8 @@ class GraphicsProgram3D:
         delta_time = self.clock.tick() / 1000.0
 
         self.angle += (pi * delta_time) * 180.0/pi
-        # if angle > 2 * pi:
-        #     angle -= (2 * pi)
+        # if self.angle > 180:
+        #     self.angle -= 180
 
 
         if self.W_key_down:
@@ -95,28 +95,21 @@ class GraphicsProgram3D:
             self.view_matrix.slide(1 * delta_time, 0, 0)
             # self.view_matrix.yaw(-180 * delta_time)
         if self.LEFT_key_down:
-            # self.view_matrix.slide(1 * delta_time, 0, 0)
-            self.view_matrix.yaw(180 * delta_time)
+            self.view_matrix.yaw(90 * delta_time)
         if self.RIGHT_key_down:
-            # self.view_matrix.slide(1 * delta_time, 0, 0)
-            self.view_matrix.yaw(-180 * delta_time)
+            self.view_matrix.yaw(-90 * delta_time)
+        # if self.UP_key_down:
+        #     self.view_matrix.pitch(90 * delta_time)
+        # if self.DOWN_key_down:
+        #     self.view_matrix.pitch(-90 * delta_time)
 
         if self.T_key_down:
             self.fov -= 1.5 * delta_time
         if self.G_key_down:
             self.fov += 1.5 * delta_time
 
-        if self.UP_key_down:
-            self.white_background = True
-        else:
-            self.white_background = False
-
-        for wall in self.walls:
-            x, y, xlength, ylength = wall
-            # self.drawWall(x, y, xlength, ylength)
-            # TODO Change to all Walls
-            if(x == 2.5): self.checkWallCollision(x, y, xlength, ylength)
-            # self.checkWallCollision(x, y, xlength, ylength)
+        
+        
     
 
     def display(self):
@@ -152,51 +145,16 @@ class GraphicsProgram3D:
         self.model_matrix.pop_matrix()
 
         # Walls
-        # TODO Enlarge Maze Paths
         for wall in self.walls:
             x, y, xlength, ylength = wall
-            self.drawWall(x, y, xlength, ylength)
+            if self.checkWallCollision(x, y, xlength, ylength):
+                self.drawWall(x, y, xlength, ylength, [0.0, 1.0, 0.0])
+            else:
+                self.drawWall(x, y, xlength, ylength)
 
+        # Rotating Cube
+        self.drawWeirdRotatingObject()
 
-        # Draw Maze Walls in Clockwise Direction starting with back wall
-        # self.drawWall_y(2,-2, 0)
-        # self.drawWall_x(0, 5, 2)
-        # self.drawWall_y(2, 4, 5)
-        # self.drawWall_x(5, 3, 4)
-        # self.drawWall_y(4, 6, 3)
-        # self.drawWall_x(3, 5, 6)
-        # self.drawWall_y(6, 8, 5)
-        # self.drawWall_x(5, 11, 8)
-        # self.drawWall_y(8, 4, 11)
-        # self.drawWall_x(11, 9, 4)
-        # self.drawWall_y(4, 6, 9)
-        # self.drawWall_x(9, 7, 6)
-        # self.drawWall_y(6, 2, 7)
-        # self.drawWall_x(7, 10, 2)
-        # self.drawWall_y(2, -6, 10)
-        # self.drawWall_x(10, 13, -6)
-        # self.drawWall_y(-6, 0, 13)
-        # self.drawWall_x(13, 15, 0)
-        # self.drawWall_y(0, -8, 15)
-        # self.drawWall_x(15, 10, -8)
-        # self.drawWall_y(-8, -10, 10)
-        # self.drawWall_x(10, 8, -10)
-        # self.drawWall_y(-10, -8, 8)
-        # self.drawWall_x(8, 5, -8)
-        # self.drawWall_y(-8, -13, 5)
-        # self.drawWall_x(5, 3, -13)
-        # self.drawWall_y(-13, -11, 3)
-        # self.drawWall_x(3, 1, -11)
-        # self.drawWall_y(-11, -9, 1)
-        # self.drawWall_x(1, 3, -9)
-        # self.drawWall_y(-9, -6, 3)
-        # self.drawWall_x(3, 1, -6)
-        # self.drawWall_y(-6, -4, 1)
-        # self.drawWall_x(1, 5, -4)
-        # self.drawWall_y(-4, -6, 5)
-        # self.drawWall_x(5, 8, -6)
-        # self.drawWall_y(-6, -2, 8)
-        # self.drawWall_x(8, 0, -2)
 
         pygame.display.flip()
 
@@ -207,50 +165,51 @@ class GraphicsProgram3D:
 
     def checkWallCollision(self, x, y, xlength, ylength):
         # Camera Collision Circle radius:
-        radius = 1
+        radius = 0.25
 
-        w = xlength / 2
-        h = ylength / 2
-        B = Point(x, y, 0)
-        C = self.view_matrix.eye
+        # Get closest point on box to camera center
+        camera = self.view_matrix.eye
 
-        D = B-C
-        P = Point(self.clamp(D.x, 0, w), self.clamp(D.y, 0, h), 0)
+        P = Point(self.clamp(camera.x, x-xlength/2, x+xlength/2),
+                self.clamp(camera.y, y-ylength/2, y+ylength/2),
+                camera.z)
 
-        V = (P - C)
-        distance = sqrt(V.x**2 + V.y**2) 
+        # Vector Camera to closest Point
+        V = P - camera
+        distance = V.__len__()
+
         collision = distance < radius
 
-        V_tmp = V
-        V_tmp.__mul__(radius/V_tmp.__len__())
+        # Hard Copy V
+        V_tmp = Vector(V.x, V.y, V.z)
+        # V_tmp.normalize()
+        mult = radius/distance
+        V_tmp.x *= mult
+        V_tmp.y *= mult
         # Used to reposition Camera
+        # V_tmp = radius Vector
         R = (V_tmp) - V
 
+
+        if(x == 4.0 and y == -2):
+            print("distance: ", distance)
+            print("Camera: ", camera.x, camera.y)
+            print("V: ", V.x, V.y)
+            print("Collision: ", collision)
+
         if(collision): 
-            # print("Collision detected")
-            # print("P: ", P.x, P.y)
-            # print("V: ", V.x, V.y)
-            # print("V_norm: ", V_tmp.x, V_tmp.y)
-            # print("R: ", R.x, R.y)
-
-            
-
+            # Reposition Camera
             self.view_matrix.eye -= R
+        #     print("Collision detected")
 
-        else: 
-            # print("\nWidth, height: ", w, h)
-            # print("B: ", B.x, B.y)
-            # print("C: ", C.x, C.y)
-            # print("D: ", D.x, D.y)
-            # print("D_len: ", sqrt(D.x**2 + D.y**2))
-            # print("Distance: ", distance)
-            # print("P: ", P.x, P.y)
-            # print("No Collision")
+
+
         return collision
 
 
-    def drawWall(self, x, y, xlength, ylength):
-        self.shader.set_solid_color(1.0, 0.0, 0.0)
+    def drawWall(self, x, y, xlength, ylength, color = [1.0, 0.0, 0.0]):
+        r, g, b = color
+        self.shader.set_solid_color(r, g, b)
         self.model_matrix.push_matrix()
         self.model_matrix.add_translation(x, y, 4.0) 
         self.model_matrix.add_scale(xlength, ylength, 8)
@@ -259,31 +218,45 @@ class GraphicsProgram3D:
         self.model_matrix.pop_matrix()
 
     # Draws a wall from startPoint to EndPoint with thickness of 0.1
-    # def drawWall_x(self, start, end, y):
-    #     length = end - start
-    #     self.shader.set_solid_color(1.0, 0.0, 0.0)
-    #     self.model_matrix.push_matrix()
-    #     self.model_matrix.add_translation(length/2 + start, y, 4.0) 
-    #     self.model_matrix.add_scale(length, 0.1, 8)
-    #     self.shader.set_model_matrix(self.model_matrix.matrix)
-    #     self.cube.draw(self.shader)
-    #     self.model_matrix.pop_matrix()
+    def drawWall_x(self, start, end, y):
+        length = end - start
+        self.shader.set_solid_color(1.0, 0.0, 0.0)
+        self.model_matrix.push_matrix()
+        self.model_matrix.add_translation(length/2 + start, y, 4.0) 
+        self.model_matrix.add_scale(length, 0.1, 8)
+        self.shader.set_model_matrix(self.model_matrix.matrix)
+        self.cube.draw(self.shader)
+        self.model_matrix.pop_matrix()
 
-    #     # Store Center Point and Width and height in Array
-    #     #                           X        Y  X-Width Y-Height
-    #     self.walls.append([length/2 + start, y, length, 0.1])
+        # Store Center Point and Width and height in Array
+        #                           X        Y  X-Width Y-Height
+        self.walls.append([length/2 + start, y, length, 0.1])
 
-    # def drawWall_y(self, start, end, x):
-    #     length = end - start
-    #     self.shader.set_solid_color(1.0, 0.0, 0.0)
-    #     self.model_matrix.push_matrix()
-    #     self.model_matrix.add_translation(x, length/2 + start, 4.0) 
-    #     self.model_matrix.add_scale(0.1, length, 8)
-    #     self.shader.set_model_matrix(self.model_matrix.matrix)
-    #     self.cube.draw(self.shader)
-    #     self.model_matrix.pop_matrix()
+    def drawWall_y(self, start, end, x):
+        length = end - start
+        self.shader.set_solid_color(1.0, 0.0, 0.0)
+        self.model_matrix.push_matrix()
+        self.model_matrix.add_translation(x, length/2 + start, 4.0) 
+        self.model_matrix.add_scale(0.1, length, 8)
+        self.shader.set_model_matrix(self.model_matrix.matrix)
+        self.cube.draw(self.shader)
+        self.model_matrix.pop_matrix()
         
-    #     self.walls.append([x, length/2 + start, 0.1, length])
+        self.walls.append([x, length/2 + start, 0.1, length])
+    
+    def drawWeirdRotatingObject(self):
+        for i in range(0, 360, 30):
+            self.shader.set_solid_color(1.0, 1.0, 0.0)
+            self.model_matrix.push_matrix()
+            self.model_matrix.add_translation(8.0, 0.0 , 3.0)  
+            self.model_matrix.add_scale(1, 1, 1)
+            self.model_matrix.add_rotation_x(i + self.angle)
+            self.model_matrix.add_rotation_z(self.angle)
+            self.shader.set_model_matrix(self.model_matrix.matrix)
+            self.cube.draw(self.shader)
+            self.model_matrix.pop_matrix()
+
+        
 
     def program_loop(self):
         exiting = False
@@ -298,8 +271,6 @@ class GraphicsProgram3D:
                         print("Escaping!")
                         exiting = True
                         
-                    if event.key == K_UP:
-                        self.UP_key_down = True
                     if event.key == K_w:
                         self.W_key_down = True
                     if event.key == K_s:
@@ -320,10 +291,12 @@ class GraphicsProgram3D:
                         self.LEFT_key_down = True
                     if event.key == K_RIGHT:
                         self.RIGHT_key_down = True
+                    if event.key == K_UP:
+                        self.UP_key_down = True
+                    if event.key == K_DOWN:
+                        self.DOWN_key_down = True
 
                 elif event.type == pygame.KEYUP:
-                    if event.key == K_UP:
-                        self.UP_key_down = False
                     if event.key == K_w:
                         self.W_key_down = False
                     if event.key == K_s:
@@ -344,6 +317,10 @@ class GraphicsProgram3D:
                         self.LEFT_key_down = False
                     if event.key == K_RIGHT:
                         self.RIGHT_key_down = False
+                    if event.key == K_UP:
+                        self.UP_key_down = False
+                    if event.key == K_DOWN:
+                        self.DOWN_key_down = False
             
             self.update()
             self.display()
